e1457a45cb2baa6f806e0e6770e1c13b86024328
Sol Battaglia <solbattaglia@gmail.com>
2021-11-26 09:04:54 -0300

    Added Test

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,12 @@ pub struct Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    // println!("With text:\n{}", contents);
 
     Ok(())
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
 }
 
 impl Config {
@@ -24,5 +27,21 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
